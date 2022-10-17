2d0f7d13ee61218547b32bb49ba7eb85c46806e4
Vi-N-Tran <Vi.n.tran.23@dartmouth.edu>
2022-10-17 01:49:30 -0400

    starter BE

--- server/server.js
+++ server/server.js
@@ -1,19 +1,16 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const PORT = 4000;
 app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
-
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-
-  });
-  console.log(`Server is running on port: ${port}`);
+app.use(bodyParser.json());
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
 });
