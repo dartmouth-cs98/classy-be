8d631956606aa59eb27b824cfa4b0c458025dae3
Vi-N-Tran <Vi.n.tran.23@dartmouth.edu>
2022-10-23 01:43:03 -0400

    starter BE typescript
    starter BE typescript
    
    starter BE typescript
    
    starter BE typescript
    
    starter BE typescript

---
+++ src/db.config.ts
@@ -0,0 +1,43 @@
+import Mongoose, { ConnectOptions } from "mongoose";
+// const Mongoose = require('mongoose');
+require('dotenv').config()
+
+let database: Mongoose.Connection;
+
+export const connect = () => {
+
+  const url = process.env.MONGO_CONNECTION_STRING;
+  console.log("from connect: process.env.MONGO_CONNECTION_STRING :::", process.env.MONGO_CONNECTION_STRING)
+
+  if (database) {
+    return;
+  }
+
+  Mongoose.connect(url!, {
+  });
+
+  database = Mongoose.connection;
+
+  database.once("open", async () => {
+    console.log("Connected to database");
+  });
+
+  database.on("error", () => {
+    console.log("Error connecting to database");
+  });
+
+};
+
+export const disconnect = () => {
+
+  if (!database) {
+    return;
+  }
+
+  Mongoose.disconnect();
+
+  database.once("close", async () => {
+    console.log("Diconnected  to database");
+  });
+
+};
