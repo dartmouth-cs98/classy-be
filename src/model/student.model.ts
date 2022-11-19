d40c40f471fb43fcdd4536d8d1e6c533ed740161
Henry Kim <henry.kim.23@dartmouth.edu>
2022-11-19 11:32:14 -0500

    update backend for friend requests

--- src/model/student.model.ts
+++ src/model/student.model.ts
@@ -4,10 +4,13 @@ export interface IStudent extends Document {
   user: Types.ObjectId;
   classYear: number; // class year of the student
   coursesTaken: Types.ObjectId[]; // courses taken by the student
-  reviews: Types.ObjectId[]; // reviews written by the student
+  courseReviews: Types.ObjectId[]; // course reviews written by the student
+  deptReviews: Types.ObjectId[]; // dept reviews written by the student
   majors: Types.ObjectId[]; // majors of the student
   minors: Types.ObjectId[]; // minors of the student
   friends: Types.ObjectId[]; // friends of the student
+  outgoingFriendRequests: Types.ObjectId[]; // friend requests sent
+  incomingFriendRequests: Types.ObjectId[]; // friend requests received
   enemies: Types.ObjectId[]; // enemies of the student
   favProfs: Types.ObjectId[]; // favorite professors of the student
   gradeThreshold: number; // grade threshold of the student
@@ -24,10 +27,13 @@ const StudentSchema: Schema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   classYear: { type: Number, required: true }, // class year of the student
   coursesTaken: [{ type: Schema.Types.ObjectId, ref: 'Course' }], // courses taken by the student
-  reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }], // reviews written by the student
+  courseReviews: [{ type: Schema.Types.ObjectId, ref: 'CourseReview' }], // reviews written by the student
+  deptReviews: [{ type: Schema.Types.ObjectId, ref: 'DeptReview' }], // reviews written by the student
   majors: [{ type: Schema.Types.ObjectId, ref: 'MajorMinor' }], // majors of the student
   minors: [{ type: Schema.Types.ObjectId, ref: 'MajorMinor' }], // minors of the student
   friends: [{ type: Schema.Types.ObjectId, ref: 'Student' }], // friends of the student
+  outgoingFriendRequests: [{ type: Schema.Types.ObjectId, ref: 'Student' }], // outgoing friend requests
+  incomingFriendRequests: [{ type: Schema.Types.ObjectId, ref: 'Student' }], // incoming friend requests
   enemies: [{ type: Schema.Types.ObjectId, ref: 'Student' }], // enemies of the student
   favProfs: [{ type: Schema.Types.ObjectId, ref: 'Professor' }], // favorite professors of the student
   gradeThreshold: { type: Number, required: true }, // grade threshold of the student
--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,11 +1,13 @@
 import { model, Schema, Model, Document } from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 export interface IUser extends Document {
   firstName: string;
   lastName: string;
-//   username: string;
-//   email: string;
-//   password: string;
+  username: string;
+  email: string;
+  password: string;
+  admin: boolean;
 
   createDate: Date,
   updatedDate: Date;
@@ -15,14 +17,38 @@ export interface IUser extends Document {
 const UserSchema: Schema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-//   username: { type: String, required: true },
-//   email: { type: String, required: true },
-//   password: { type: String, required: true },
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  admin: { type: Boolean },
 
   createDate: { type: Date, default: Date.now },
   updatedDate: { type: Date, default: Date.now },
   timestamps: { createDate: Date, updatedDate: Date }
 }
 );
+
+UserSchema.pre('save', async function beforeUserSave(next) {
+    // get access to the user model
+    const user = this;
+  
+    if (!user.isModified('password')) return next();
+  
+    try {
+      // salt, hash, then set password to hash
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(user.password, salt);
+      user.password = hash;
+      return next();
+    } catch (error: any) {
+      return next(error);
+    }
+  });
+  
+  // note use of named function rather than arrow notation, required here
+  UserSchema.methods.comparePassword = async function comparePassword(candidatePassword) {
+    const comparison = await bcrypt.compare(candidatePassword, this.password);
+    return comparison;
+  };
 
 export const UserModel: Model<IUser> = model<IUser>('User', UserSchema);
