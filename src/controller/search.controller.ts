06cc6e48ac6f3631507ff9805d187c0ce3965770
Alex Feng <Alex.feng.23@dartmouth.edu>
2023-02-12 20:51:45 -0500

    variable renaming

--- src/controller/search.controller.ts
+++ src/controller/search.controller.ts
@@ -10,9 +10,9 @@ export const getSearch = async (searchString: string) => {
     //     return /\d/.test(myString);
     // }
 
-    // split searchString into numeric (courseNumber) and nonnumeric (courseDept) strings and query based on strings
+    // split searchString into numeric (courseNumber) and alpha (courseDept) strings and query based on strings
     // see https://stackoverflow.com/questions/49887578/splitting-a-string-with-a-decimal-number-and-some-characters for match() details
-    const nonnumeric = searchString.match(/[a-zA-Z ]+/ig);
+    const alpha = searchString.match(/[a-zA-Z ]+/ig);
     const numeric = searchString.match(/[\d\.?]+/ig);
 
     // if searchString has numeric values search from courseDept + courseNum
@@ -20,13 +20,13 @@ export const getSearch = async (searchString: string) => {
         // const alpha = searchString.match(/[a-zA-Z]+);
         // const num = searchString.match(/[a-zA-Z]+|[\d\.?]+/ig);
         // if (result) {
-        //     nonnumeric = result[0];
+        //     alpha = result[0];
         //     numeric = result[1].replace(/^0+/, ""); // remove all leading 0s in numeric
         // }
 
         const num = numeric[0].replace(/^0+/, ""); // remove all leading 0s in numeric
 
-        if (nonnumeric && nonnumeric[0] && num) {
+        if (alpha && alpha[0] && num) {
             console.log("Searching coursedeptnum");
 
             const deptNumSearch = await CourseModel.aggregate(
@@ -38,7 +38,7 @@ export const getSearch = async (searchString: string) => {
                           'must': [
                             {
                               'text': {
-                                'query': nonnumeric[0], 
+                                'query': alpha[0], 
                                 'path': {
                                   'wildcard': '*'
                                 }
@@ -65,8 +65,8 @@ export const getSearch = async (searchString: string) => {
 
     }
 
-    // if nonnumeric is exactly 3 or 4 characters, search from departments
-    else if (nonnumeric && (nonnumeric[0].length == 4 || nonnumeric[0].length == 3)) {
+    // if alpha is exactly 3 or 4 characters, search from departments
+    if (alpha && (alpha[0].length == 4 || alpha[0].length == 3)) {
         console.log("Searching dept");
 
         const deptSearch = await CourseModel.aggregate(
@@ -75,7 +75,7 @@ export const getSearch = async (searchString: string) => {
                   '$search': {
                     'index': 'coursesearch', 
                     'text': {
-                        'query': nonnumeric[0],
+                        'query': alpha[0],
                         'path': 'courseDept'
                     }
                   }
@@ -94,15 +94,15 @@ export const getSearch = async (searchString: string) => {
     }
 
     
-    // if searchString doesn't contain any numbers and is not 4 characters long and contains any letters/nonnumerics, search from course titles
-    else if (nonnumeric && nonnumeric[0]) {
+    // if searchString contains any alphas, search from course titles
+    if (alpha && alpha[0]) {
         const nameSearch = await CourseModel.aggregate(
             [
                 {
                   '$search': {
                     'index': 'coursesearch', 
                     'autocomplete': {
-                        'query': nonnumeric[0],
+                        'query': alpha[0],
                         'path': 'courseTitle',
                     }
                   }
@@ -117,7 +117,7 @@ export const getSearch = async (searchString: string) => {
               ]
         );
         return nameSearch;
-        // console.log(nonnumeric[0]);
+        // console.log(alpha[0]);
         // console.log("here")
     }
 
